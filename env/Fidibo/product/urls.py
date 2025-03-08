045eb188469bef7de8b71cf8188881ef858eb470
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-03-08 13:23:18 +0330

    set Dorrci func

--- env/Fidibo/product/urls.py
+++ env/Fidibo/product/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import product_hello_page,display_name
+from .views import product_hello_page,display_name,download_Dorcci
 urlpatterns = [
     path("Hello/", product_hello_page),
+    path("Dorrci/", download_Dorcci),
     path("<str:name>", display_name),
+
 
 ]
--- env/Fidibo/product/views.py
+++ env/Fidibo/product/views.py
@@ -1,4 +1,4 @@
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, FileResponse
 import os
 
 
@@ -8,6 +8,17 @@ def product_hello_page(request):
 
 def display_name(request, name):
     return HttpResponse(f"Hello {name} welcome!")
+
+
+
+def download_Dorcci(request):
+    file_music = os.path.join("/home/amirykta/Desktop/Fidibo/env/Fidibo/product/10.Dorcci - Addi (320).mp3")
+    return FileResponse(
+        open(file_music, "rb"),
+        as_attachment=True
+    )
+    
+
 
 
 
