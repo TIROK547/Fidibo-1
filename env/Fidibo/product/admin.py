e71c7ac3b5f8b738257c1dbd15a643cf499ec2a1
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-03-09 15:55:12 +0330

    set models in admin.py

--- env/Fidibo/product/admin.py
+++ env/Fidibo/product/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User,Profile,Authur,Book
+
+admin.site.register(User)
+admin.site.register(Profile)
+admin.site.register(Authur)
+admin.site.register(Book)
 
 # Register your models here.
